dfa0be4329f0a23faa77abe0552781deeb5c3c94
rj7075 <rj7075yadav.com>
2024-08-30 10:32:27 +0530

    config done

--- Pacage.js
+++ Pacage.js
@@ -1,2 +1,5 @@
 //  It is contain the details about your projects
-console.log("pacage.json");
+const colors = require("colors");
+console.log("pacage.json".red); // contain all dependency of projects;
+
+console.log("Hello".green);
